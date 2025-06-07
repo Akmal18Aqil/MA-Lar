2d91476ea0cea85f14f53f22b8203c91b755db5e
Cyber <cyber.annur2@gmail.com>
2025-06-07 22:50:29 +0700

    more

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,29 +6,24 @@
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
         </div>
+
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="dropdown {{ Request::is('dashboard') ? 'active' : '' }}">
+            <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
                 <a href="{{ route('admin.dashboard') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
-            <li class="menu-header">Starter</li>
-            <li class="dropdown {{ Request::is('mahasantri*') ? 'active' : '' }}">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Mahasantri</span></a>
-                <ul class="dropdown-menu">
-                    <li class="{{ Request::is('mahasantri') ? 'active' : '' }}"><a class="nav-link" href="{{ route('mahasantri.index') }}">List Mahasantri</a></li>
-                    <li class="{{ Request::is('mahasantri/create') ? 'active' : '' }}"><a class="nav-link" href="{{ route('mahasantri.create') }}">Add Mahasantri</a></li>
-                </ul>
+
+            <li class="menu-header">Mahasantri</li>
+            <li class="{{ Request::is('mahasantri') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('mahasantri.index') }}"><i class="fas fa-user-graduate"></i><span>Mahasantri</span></a>
             </li>
-        </ul>
-        <ul class="sidebar-menu">
+            
+
             <li class="menu-header">User Management</li>
-            <li class="dropdown {{ Request::is('admin/users*') ? 'active' : '' }}">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-users"></i> <span>Users</span></a>
-                <ul class="dropdown-menu">
-                    <li class="{{ Request::is('admin/users') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin.users.index') }}">List Users</a></li>
-                    <li class="{{ Request::is('admin/users/create') ? 'active' : '' }}"><a class="nav-link" href="{{ route('admin.users.create') }}">Add User</a></li>
-                </ul>
+            <li class="{{ Request::is('admin/users') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('admin.users.index') }}"><i class="fas fa-users"></i><span>Users</span></a>
             </li>
+            
         </ul>
     </aside>
 </div>

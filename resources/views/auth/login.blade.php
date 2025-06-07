f03a0ef08ec890d9e1125d1c0d82e4e78c505af0
Cyber <cyber.annur2@gmail.com>
2025-06-07 20:38:06 +0700

    feat: login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,129 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Login &mdash; Stisla</title>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('library/@fortawesome/fontawesome-free/css/all.min.css') }}">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <!-- CSS Libraries -->
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/components.css') }}">
+
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-94034622-3');
+    </script>
+    <!-- /END GA --></head>
+
+<body>
+<div id="app">
+    <section class="section">
+        <div class="d-flex flex-wrap align-items-stretch">
+            <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
+                <div class="p-4 m-3">
+                    <img src="{{ asset('img/stisla-fill.svg') }}" alt="logo" width="80" class="shadow-light rounded-circle mb-5 mt-2">
+                    <h4 class="text-dark font-weight-normal">Welcome to <span class="font-weight-bold">Stisla</span></h4>
+                    <p class="text-muted">Before you get started, you must login or register if you don't already have an account.</p>
+
+                    <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+                        @csrf
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" tabindex="1" required autofocus value="{{ old('email') }}">
+                            @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <div class="d-block">
+                                <label for="password" class="control-label">Password</label>
+                                <div class="float-right">
+                                    @if (Route::has('password.request'))
+                                        <a href="{{ route('password.request') }}" class="text-small">
+                                            Forgot Password?
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" tabindex="2" required>
+                            @error('password')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+                                <label class="custom-control-label" for="remember-me">Remember Me</label>
+                            </div>
+                        </div>
+
+                        <div class="form-group text-right">
+                            <button type="submit" class="btn btn-primary btn-lg btn-icon icon-right" tabindex="4">
+                                Login
+                            </button>
+                        </div>
+
+                        <div class="mt-5 text-center">
+                            Don't have an account? <a href="{{ route('register') }}">Create One</a>
+                        </div>
+                    </form>
+
+                    <div class="text-center mt-5 text-small">
+                        Copyright &copy; Stisla 2018. Made with 💙 by Stisla
+                        <div class="mt-2">
+                            <a href="#">Privacy Policy</a>
+                            <div class="bullet"></div>
+                            <a href="#">Terms of Service</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="{{ asset('img/unsplash/login-bg.jpg') }}">
+                <div class="absolute-bottom-left index-2">
+                    <div class="text-light p-5 pb-2">
+                        <div class="mb-5 pb-3">
+                            <h1 class="mb-2 display-4 font-weight-bold">Good Morning</h1>
+                            <h5 class="font-weight-normal text-muted-transparent">Bali, Indonesia</h5>
+                        </div>
+                        Photo by <a class="text-light bb" target="_blank" href="https://unsplash.com/photos/a8lTjWJGBgM">ahhfoo</a> on <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a>
+                    </div>
+                </div>
+            </div>
         </div>
+    </section>
+</div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+<!-- General JS Scripts -->
+<script src="{{ asset('library/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('library/popper.js/dist/umd/popper.js') }}"></script>
+<script src="{{ asset('library/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('library/nicescroll/jquery.nicescroll.min.js') }}"></script>
+<script src="{{ asset('library/moment/min/moment.min.js') }}"></script>
+<script src="{{ asset('js/stisla.js') }}"></script>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+<!-- JS Libraries -->
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+<!-- Page Specific JS File -->
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+<!-- Template JS File -->
+<script src="{{ asset('js/scripts.js') }}"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
+</body>
+</html>
